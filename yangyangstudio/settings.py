f241de999ad5dc0035005f0fc907cd99ade913e4
Derong Qiu <eddiechiu77@sina.com>
2017-12-03 12:08:20 +0800

    first push

--- yangyangstudio/settings.py
+++ yangyangstudio/settings.py
@@ -133,7 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 
 STATIC_URL = '/static/'
-#  STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static').replace('\\', '/'),
 )
